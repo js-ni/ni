e1cb49309f669c48a5df10d05679f30a456a9de7
Paulo McNally <paulomcnally@gmail.com>
2018-02-12 09:35:11 -0600

    Add Coffee JS - Febrero 2018

--- src/views/Events/events.js
+++ src/views/Events/events.js
@@ -1,18 +1,20 @@
 export default [
-  {
-    address: 'Managua',
-    lat: 12.1493444,
-    lon: -86.2744439,
-    name: 'Hackathon Nicaragua 2017',
-    start_date: '2017-11-25 00:00:00',
-    venue_name: 'Centro De Convenciones Olof Palme'
-  },
-  {
-    address: 'KM 8.5 Carretera Masaya, De semáforos Santo Domingo 2 c oeste 25 vrs sur, Mirador Norte Santo Domingo, Managua 14236',
-    lat: 12.093037,
-    lon: -86.2433097,
-    name: 'CoffeeJS',
-    start_date: null,
-    venue_name: 'Rain Agency'
-  }
+{
+  address: 'Por donde fué la alcaldía de Tipitapa, Tipitapa, Managua, Nicaragua.',
+  lat: 12.201777,
+  lon: -86.095618,
+  name: 'CoffeeJS - Febrero 2018',
+  start_date: '2018-02-24 14:00:00',
+  venue_name: 'Buffet Restaurante Espino Negro',
+  facebook_event_link: 'https://www.facebook.com/events/151217388910423/'
+},
+{
+  address: 'Managua',
+  lat: 12.1493444,
+  lon: -86.2744439,
+  name: 'Hackathon Nicaragua 2017',
+  start_date: '2017-11-25 00:00:00',
+  venue_name: 'Centro De Convenciones Olof Palme',
+  facebook_event_link: 'https://www.facebook.com/events/1738624739533422/'
+}
 ];
