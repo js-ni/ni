308e9ced435e82a71c36221b79597c054008ad58
Paulo McNally <paulomcnally@gmail.com>
2018-03-29 11:30:40 -0600

    Add basic blog component

---
+++ src/components/Blog.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+
+class Blog extends Component {
+  render() {
+    return (
+      <Wrapper>
+
+      </Wrapper>
+    );
+  }
+}
+
+const Wrapper = styled.div`
+  width: 948px;
+  margin 0 auto;
+  position: relative;
+  overflow: hidden;
+  display: flex;
+  background-color: #ffffff;
+`;
+
+export default Blog;
