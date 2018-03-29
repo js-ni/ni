c5fe2b96c48904ef2010396f0cd44ebb0ac43647
Paulo McNally <paulomcnally@gmail.com>
2018-03-29 11:30:24 -0600

    Add reference to events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Header from './components/Header';
+import Blog from './components/Blog';
+import Events from './components/Events';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      events: {}
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      events: ReactDOM.findDOMNode(this.refs.events)
+    })
+  }
+
   render() {
     return (
       <div>
-        <Header />
+        <Header events={ this.state.events } />
+        <Blog />
+        <Events ref={ 'events' } />
       </div>
     );
   }
