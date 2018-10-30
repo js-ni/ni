8d42848417fcdcfd903665cbf1712019f5f66c41
Hosmel Quintana <hosmelq@gmail.com>
2018-10-30 06:57:21 -0600

    add new medium posts package
    Closes #15

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
-import Welcome from './components/Welcome';
-import Blog from './components/Blog';
+import React from 'react';
+import {Box} from 'rebass';
+import MediumPosts from '@js-ni/medium-posts';
+
 import Events from './components/Events';
 import Footer from './components/Footer';
+import Header from './components/Header';
+import Welcome from './components/Welcome';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <Header />
-        <Welcome />
-        <Blog />
-        <Events />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <>
+      <Header />
+      <Welcome />
+      <Box bg="#eff1f5">
+        <Box m="0 auto" px={48} py={42} width={948}>
+          <MediumPosts />
+        </Box>
+      </Box>
+      <Events />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
