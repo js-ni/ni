dd951ecd7306491d187615d260ac851bde7baa8b
Christopher Ramírez <chris.ramirezg@gmail.com>
2018-11-13 09:35:15 -0600

    Add publicRepos funtion to GithubOrg class

--- src/components/Members/GithubOrg/index.js
+++ src/components/Members/GithubOrg/index.js
@@ -25,4 +25,27 @@ export default class GithubOrg {
       return cache.KNOWN_MEMBERS;
     });
   };
+
+  /**
+   * Returns a promise which resolves in an array of org public repositories.
+   * The array is sorted by the number of repository stargazers
+   */
+  publicRepos = () => {
+    const API_ENDPOINT = `https://api.github.com/orgs/${
+      this.organization
+    }/repos`;
+    const request = fetch(API_ENDPOINT);
+
+    return request.then(response => {
+      if (response.ok) {
+        const repos = response.json();
+
+        return repos.sort((a, b) => {
+          return b.stargazers_count - a.stargazers_count
+        })
+      }
+
+      return []
+    });
+  }
 }
