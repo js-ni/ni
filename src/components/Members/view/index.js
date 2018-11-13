1df478da29347f1b68e6178907c2ecd3ebd4b9c6
Christopher Ramírez <chris.ramirezg@gmail.com>
2018-11-13 07:44:48 -0600

    Members.view components are now functional

--- src/components/Members/view/index.js
+++ src/components/Members/view/index.js
@@ -8,19 +8,16 @@ import {Box} from 'rebass';
  * Props required:
  * - member: A dictionay of a Github profile that has at least the avatar and username
  */
-export class MemberProfile extends React.Component {
-    render() {
-        return (
-            <a href={this.props.member.html_url}
-               title={this.props.member.login}
-               style={{display: "inline-block"}}>
-                <img src={this.props.member.avatar_url} width="64px"
-                     alt={this.props.member.login} />
-            </a>
-        )
-    }
+export function MemberProfile(props) {
+    return (
+        <a href={props.member.html_url}
+           title={props.member.login}
+           style={{display: "inline-block"}}>
+            <img src={props.member.avatar_url} width="64px"
+                 alt={props.member.login} />
+        </a>
+    )
 }
-
 
 /**
  * Displays a list of github profiles. Each profile is displayed in a MemberProfile element
@@ -28,21 +25,17 @@ export class MemberProfile extends React.Component {
  * Props required:
  * - members: An array of github dictinaries profiles
  */
-export class OrganizationMembers extends React.Component {
-    render() {
-        return (
-            <Box px={20}
-                style={{display: 'flex', flexDirection: 'row', justifyContent: "center", flexWrap: "wrap"}}>
-                {
-                    this.props.members.map(member => (
-                        <Box mx={2} my={2} style={{borderRadius: "8px", overflow: "hidden"}} key={`member-${member.login}`}>
-                            <MemberProfile member={member} />
-                        </Box>
-                    ))
-                }
-            </Box>
-        )
-    }
+export function OrganizationMembers(props) {
+    return (
+        <Box px={20}
+            style={{display: 'flex', flexDirection: 'row', justifyContent: "center", flexWrap: "wrap"}}>
+            {
+                props.members.map(member => (
+                    <Box mx={2} my={2} style={{borderRadius: "8px", overflow: "hidden"}} key={`member-${member.login}`}>
+                        <MemberProfile member={member} />
+                    </Box>
+                ))
+            }
+        </Box>
+    )
 }
-
-
