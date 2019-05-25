57f8f19f3f9801243db054eb91a8535474e4baaf
Hosmel Quintana <hosmelq@gmail.com>
2019-05-25 12:31:59 -0600

    Updates

---
+++ precache-manifest.3d0524a819fd92962a3770b5fc46a869.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ee0e55d3391d325daeb442e2bfab3ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "206d86cefb9cf54da70c",
+    "url": "/static/css/2.617501a0.chunk.css"
+  },
+  {
+    "revision": "10720ee13a3871868b51",
+    "url": "/static/css/main.c91278da.chunk.css"
+  },
+  {
+    "revision": "206d86cefb9cf54da70c",
+    "url": "/static/js/2.bbad7d0c.chunk.js"
+  },
+  {
+    "revision": "10720ee13a3871868b51",
+    "url": "/static/js/main.0648f091.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e3baf27897b91c4874f9768414882da6",
+    "url": "/static/media/computer.e3baf278.jpg"
+  },
+  {
+    "revision": "ec85934141a9e1078b9d7a887b9e309b",
+    "url": "/static/media/event.ec859341.jpg"
+  }
+]);
