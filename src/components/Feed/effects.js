22bf8c9c918e4cea3f738062560f8acc77213696
Christopher Ramirez <chris.ramirezg@gmail.com>
2019-07-15 02:59:27 -0600

    Implement htmlToText function
    This function will be used to convert content and description HTML to text.
    In the future, this function may be refactored into a separated module.

--- src/components/Feed/effects.js
+++ src/components/Feed/effects.js
@@ -2,6 +2,21 @@ import React, {useState, useEffect} from 'react';
 
 const RSS2Json_ENDPOINT = 'https://api.rss2json.com/v1/api.json';
 
+/**
+ * Transform a HTML content into plain text.
+ *
+ * @param {String} htmlSource HTML Content to transform
+ * @returns {String} Plain text representation of htmlSource
+ */
+const htmlToText = function htmlToText(htmlSource) {
+  const doc = new DOMParser().parseFromString(htmlSource, 'text/html');
+  let plain = doc.body.textContent || '';
+
+  // Remove URLs leaved by DOMParser
+  plain = plain.replace(/https?:\/\/.*?\/[a-z0-9/#:?&_-]*/g, '');
+  return plain;
+};
+
 /** 
  Effect to retrieve a RSS feed as an array of objects.
  Each object correspond to a publication present in the feed.
