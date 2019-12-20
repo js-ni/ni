0136bfff4c3727c9701a17deedf5adfabc56dd71
Paulo McNally <paulomcnally@gmail.com>
2019-12-20 15:42:24 -0600

    Fix #32

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -5,9 +5,7 @@ import {Box, Card, Heading, Image, Link, Text} from 'rebass';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 function buildStaticMapUrl(lat, lng) {
-  return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=16&size=580x300&markers=color:red%7Clabel:%7C12.136062922753,-86.266359686852&key=${
-    process.env.REACT_APP_GOOGLE_MAP_KEY
-  }`;
+  return `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=16&size=580x300&markers=color:red%7Clabel:%7C12.136062922753,-86.266359686852&key=AIzaSyC5YSkpeBuJyiBp0BnkWBW6Es3CQPymD84`;
 }
 
 function EventCard(props) {
