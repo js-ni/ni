225c0a0a6d4c7c4ca8dc82bd529a16218f7ff66f
Hosmel Quintana <hosmelq@gmail.com>
2020-02-12 16:37:03 -0600

    update Event Card design

--- src/components/EventCard/index.js
+++ src/components/EventCard/index.js
@@ -1,7 +1,16 @@
 import {format, parseISO} from 'date-fns';
 import esLocale from 'date-fns/locale/es';
 import React from 'react';
-import {Box, Heading, Text} from '@chakra-ui/core';
+import {
+  Badge,
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from '@chakra-ui/core';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 
 function buildStaticMapUrl(lat, lng) {
@@ -14,47 +23,83 @@ export default function EventCard(props) {
   let {latitude, longitude} = location;
 
   return (
-    <Box bg="white" rounded={4} overflow="hidden">
-      <img alt={props.name} src={buildStaticMapUrl(latitude, longitude)} />
-      <Box color="#777" p={3}>
-        <Heading as="h3" size="md">
-          <Box
-            as="a"
-            color="#4183c4"
-            href={`https://www.facebook.com/events/${props.id}`}
+    <Flex
+      backgroundColor="white"
+      borderRadius="md"
+      direction="column"
+      overflow="hidden"
+      shadow="md"
+    >
+      <Image
+        alt={props.name}
+        src={buildStaticMapUrl(latitude, longitude)}
+        width="100%"
+      />
+      <Flex direction="column" flex={1} p={4}>
+        <Link>
+          <Heading
+            as="h2"
+            color="gray.900"
+            fontWeight="bold"
+            fontFamily="heading"
+            lineHeight="shorter"
+            size="lg"
           >
             {props.name}
-          </Box>
-        </Heading>
-
-        <Box color="gray.600">
-          <Text mt={2}>
-            <FontAwesomeIcon fixedWidth icon={['far', 'calendar-alt']} />
-            {format(parseISO(props.start_time), 'MMMM d, y', {
-              locale: esLocale,
-            })}
+          </Heading>
+        </Link>
+        <Flex
+          alignItems="center"
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          mb={8}
+          mt={1}
+        >
+          <Text color="gray.700">
+            {place.name}, {location.city}
           </Text>
-          <Text>
-            <FontAwesomeIcon fixedWidth icon={['far', 'clock']} />
-            {format(parseISO(props.start_time), 'hh:mm a')} -{' '}
-            {format(parseISO(props.end_time), 'hh:mm a')}
-          </Text>
-          <Text>
-            <Box
-              as="a"
-              color="gray.600"
-              href={`https://www.google.com/maps/@${latitude},${longitude},17z`}
+          <Button
+            as="a"
+            href={`https://www.google.com/maps/@${latitude},${longitude},17z`}
+            opacity={1}
+            size="xs"
+            variant="solid"
+            variantColor="cyan"
+          >
+            <FontAwesomeIcon fixedWidth icon={['fas', 'map-marker-alt']} />
+            Mapa
+          </Button>
+        </Flex>
+        <Flex
+          alignItems="center"
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          mt="auto"
+        >
+          <Box>
+            <Text fontSize="sm">Fecha</Text>
+            <Text
+              fontSize="xs"
+              fontWeight="bold"
+              mt={1}
+              textTransform="capitalize"
             >
-              <FontAwesomeIcon fixedWidth icon={['far', 'map']} />
-              {place.name}
-            </Box>
-          </Text>
-          <Text>
-            <FontAwesomeIcon fixedWidth icon={['fas', 'globe-americas']} />
-            {location.city}, {location.country}
-          </Text>
-        </Box>
-      </Box>
-    </Box>
+              {format(parseISO(props.start_time), 'MMMM d, y', {
+                locale: esLocale,
+              })}
+            </Text>
+          </Box>
+          <Box>
+            <Text fontSize="sm">Hora</Text>
+            <Badge variant="subtle" variantColor="green">
+              {format(parseISO(props.start_time), 'hh:mm a')} -{' '}
+              {format(parseISO(props.end_time), 'hh:mm a')}
+            </Badge>
+          </Box>
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
--- src/icons.js
+++ src/icons.js
@@ -1,14 +1,6 @@
 import {library} from '@fortawesome/fontawesome-svg-core';
 
-import {
-  faCalendarAlt as farCalendarAlt,
-  faClock as farClock,
-  faMap as farMap,
-} from '@fortawesome/free-regular-svg-icons';
-import {faGlobeAmericas} from '@fortawesome/free-solid-svg-icons';
-
-// free regular
-library.add(farCalendarAlt, farClock, farMap);
+import {faMapMarkerAlt as fasMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
 
 // free solid
-library.add(faGlobeAmericas);
+library.add(fasMapMarkerAlt);
